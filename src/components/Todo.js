dcdd2315ae42ff6e546d07b4cfea585e8f008811
lelhulu <71341997+lelhulu@users.noreply.github.com>
2024-06-13 13:52:50 -0400

    Finished

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import trashIcon from './Database Project EERD update.jpg'; 
 
-export const Todo = () => {
+export const Todo = ({ task, deleteTodo, toggleComplete }) => {
   return (
-    <div>Todo</div>
-  )
-}
+    <div className="Todo">
+      <p
+        className={`${task.completed ? "completed" : "incompleted"}`}
+        onClick={() => toggleComplete(task.id)}
+      >
+        {task.task}
+      </p>
+      <div>
+        <img 
+          src={trashIcon} 
+          alt="Delete" 
+          onClick={() => deleteTodo(task.id)} 
+          style={{ cursor: 'pointer', marginLeft: '10px' }} 
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
